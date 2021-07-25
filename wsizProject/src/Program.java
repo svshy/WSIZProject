ea59f1265dc28508f19ad7c0a2bbf574fa212ef7
domin <dominik.such.96@gmail.com>
2021-07-25 17:02:40 +0200

    1.1

--- wsizProject/src/Program.java
+++ wsizProject/src/Program.java
@@ -1,5 +1,6 @@
 public class Program {
     public static void main(String[] args) {
-
+        Employee employee = new Employee("Jan", "Kowalski", 25);
+        System.out.println(employee);
     }
 }
